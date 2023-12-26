98e7406a86740d5cbbee18621d69bead95637459
nikelear <nike.lear@gmail.com>
2023-12-26 12:37:51 +0900

    make init.ps1

---
+++ .config/nvim/bin/init.ps1
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir -p "${HOME}/.cache/dpp/repos/github.com/"
+# cd "${HOME}/.cache/dpp/repos/github.com/"
+
+# mkdir Shougo
+# mkdir vim-denops
+
+# cd ./Shougo
+# git clone https://github.com/Shougo/dpp.vim
+
+# git clone https://github.com/Shougo/dpp-ext-installer
+# git clone https://github.com/Shougo/dpp-protocol-git
+# git clone https://github.com/Shougo/dpp-ext-lazy
+# git clone https://github.com/Shougo/dpp-ext-toml
+
+# cd ../vim-denops
+# git clone https://github.com/vim-denops/denops.vim
