38d0c1e421f94f641d58ca2673239efff3fc03fc
nikelear <nike.lear@gmail.com>
2023-12-27 14:12:01 +0900

    fix alias

--- .config/nvim/lua/alias.lua
+++ .config/nvim/lua/alias.lua
@@ -1,2 +1,2 @@
-vim.api.nvim_create_user_command("call dpp#async_ext_action('installer', 'install')", "dppins", {})
-vim.api.nvim_create_user_command("call dpp#async_ext_action('installer', 'update')", "dppupd", {})
+vim.api.nvim_create_user_command('Di','call dpp#async_ext_action("installer", "install")', {})
+vim.api.nvim_create_user_command('Du','call dpp#async_ext_action("installer", "update")', {})
