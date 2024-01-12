cdbc2ac62c18a4eb922a7ec69fd72f17ea421589
nikelear <nike.lear@gmail.com>
2024-01-12 16:05:25 +0900

    sdjfij

--- .config/nvim/lua/plugin-config/comment.lua
+++ .config/nvim/lua/plugin-config/comment.lua
@@ -1,7 +1,7 @@
 return function ()
   require("Comment").setup()
   local opt = { noremap = true, silent = true }
-  vim.keymap.set("n", "<C-/>", "gcc", {})
-  vim.keymap.set("v", "<C-/>", "gc", {})
-  vim.keymap.set('i', '<C-/>', [[jjj<Esc>:normal gcc<CR>:s/jjj//g<CR>A]], {})
+  vim.keymap.set("n", "<C-q>", "<Plug>(comment_toggle_linewise_current)", {})
+  vim.keymap.set("v", "<C-q>", "<Plug>(comment_toggle_linewise_visual)", {})
+  vim.keymap.set('i', '<C-q>', [[jjj<Esc>:normal gcc<CR>:s/jjj//g<CR>A]], {})
 end
