6b3a4f230a7b3de42c2fafeae2580f23ead3d487
nikelear <nike.lear@gmail.com>
2024-01-22 09:23:13 +0900

    edit git-config and fix init.sh

--- bin/init.sh
+++ bin/init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
-DIR=$(dirname $(dirname "$0"))
-ln -sn "${DIR}/config/bash/.bash_profile" "${HOME}/.bash_profile"
-ln -sn "${DIR}/config/bash/.bashrc" "${HOME}/.bashrc"
-ln -sn "${DIR}/config" "${HOME}/.config"
-ln -sn "${DIR}/.zshenv" "${HOME}/.zshenv"
+DIR=$(dirname $(cd $(dirname $0) && pwd))
+ln -snf "${DIR}/config/bash/.bash_profile" "${HOME}/.bash_profile"
+ln -snf "${DIR}/config/bash/.bashrc" "${HOME}/.bashrc"
+ln -snf "${DIR}/config" "${HOME}/.config"
+ln -snf "${DIR}/.zshenv" "${HOME}/.zshenv"
