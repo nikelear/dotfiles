6c39c2b62433a14f02eca24fcec21b819486195a
nikelear <nike.lear@gmail.com>
2024-01-22 16:36:46 +0900

    win settings

--- bin/winlink.ps1
+++ bin/winlink.ps1
@@ -1,6 +1,4 @@
-New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\config" -Target "$env:USERPROFILE\dotfiles\.config"
-New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\Documents\PowerShell\" -Target "$env:USERPROFILE\.config\powershell"
+$parentDirectory = Split-Path $(Split-Path $PSCommandPath)
 
-$filePath = "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
-if (Test-Path $filePath) {Remove-Item $filePath}
-New-Item -ItemType HardLink -Path $filePath -Target "$env:USERPROFILE\.config\windows-terminal\settings.json"
+New-Item -ItemType SymbolicLink -Target $parentDirectory\config -Path $env:USERPROFILE\.config 
+New-Item -ItemType SymbolicLink -Target "$env:USERPROFILE\.config\powershell" -Path "$env:USERPROFILE\Documents\PowerShell\" 
