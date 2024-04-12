44d53dfaff7f3b4b3381d7d4ee373b1bb741dc80
nikelear <nike.lear@gmail.com>
2024-04-12 14:12:22 +0900

    edit pwsh

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,16 +1,10 @@
 $ENV:LANG = "en_US.UTF-8"
-# prompt
-$ENV:STARSHIP_CONFIG = "${HOME}\.config\starship.toml"
 
 # XDG
 $ENV:XDG_CONFIG_HOME="${HOME}\.config"
 $ENV:XDG_CACHE_HOME="${HOME}\.cache"
 $ENV:XDG_DATA_HOME="${HOME}\.local\share"
 $ENV:XDG_STATE_HOME="${HOME}\.local\state"
-
-# vim
-$SHOME="${HOME}" -replace '\\', '/'
-$ENV:VIMINIT=":source ${SHOME}/.config/nvim/init.vim"
 
 # fzf
 $ENV:FZF_DEFAULT_COMMAND='rg --files --hidden --follow --glob "!.git/*"'
@@ -23,26 +17,6 @@ $ENV:GOPATH="${HOME}\.local\share\go"
 
 # npm
 $ENV:NPM_CONFIG_USERCONFIG="${HOME}/.config/npm/npmrc"
-
-Set-Alias wget Invoke-WebRequest
-Function SayHello($name){
-    Write-Host "Hello $name!"
-}
-
-Function fzf_cd_ghq() {
-    $root="$(ghq root)"
-    $repo="$(ghq list | fzf --reverse --preview="ls -AF --color=always ${root}/{1}")"
-    if ( ! $repo -eq "") { 
-        $dir="${root}/${repo}"
-        cd "${dir}"
-    }
-}
-Set-PSReadLineKeyHandler -Chord Ctrl+g -ScriptBlock {
-    fzf_cd_ghq
-    [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
-}
-
-Invoke-Expression (&starship init powershell)
 
 $LOCALCONF="$HOME\Documents\PowerShell\local.ps1"
 if ( Test-Path $LOCALCONF ) { . $LOCALCONF } else { echo "boke" }
