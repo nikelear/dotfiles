f45554dd3fa6abb752933b0d0c929b939fe27bde
nikelear <nike.lear@gmail.com>
2024-04-25 15:52:48 +0900

    edit config

--- config/sh/env.sh
+++ config/sh/env.sh
@@ -3,17 +3,21 @@ export RSYNC_RSH=ssh
 # autorun-editor
 export EDITOR=$(command -v nvim || command -v vim)
 
-# npm
-export NPM_CONFIG_USERCONFIG="${XDG_CONFIG_HOME}/npm/npmrc"
-
-# node
-export NODE_REPL_HISTORY="${XDG_STATE_HOME}/node_history"
-
 # XDG
 export XDG_CONFIG_HOME="${HOME}/.config"
 export XDG_CACHE_HOME="${HOME}/.cache"
 export XDG_DATA_HOME="${HOME}/.local/share"
 export XDG_STATE_HOME="${HOME}/.local/state"
+
+# npm
+export NPM_CONFIG_PREFIX="${XDG_DATA_HOME}/npm"
+export NPM_CONFIG_USERCONFIG="${XDG_CONFIG_HOME}/npm/npmrc"
+
+# node
+export NODE_REPL_HISTORY="${XDG_STATE_HOME}/node_history"
+
+# docker
+export DOCKER_CONFIG="${XDG_DATA_HOME}/docker"
 
 # ubuntu
 export skip_global_compinit=1
---
+++ config/zsh/rc/ex.sh
@@ -0,0 +1,17 @@
+[ ! command -v git &> /dev/null ] && exit 0
+
+if [ ! -d "${XDG_DATA_HOME}/zsh-abbr" ] && [ ! -d "${XDG_DATA_HOME}/fzf-tab-completion" ]; then
+  [ timeout 5 git ls-remote "https://github.com/zenn-dev/zenn-community" &> /dev/null ] && exit 0
+fi
+
+if [ ! -d "${XDG_DATA_HOME}/zsh-abbr" ]; then
+  (cd "${XDG_DATA_HOME}" && git clone https://github.com/olets/zsh-abbr)
+fi
+
+if [ ! -d "${XDG_DATA_HOME}/fzf-tab-completion" ]; then
+  (cd "${XDG_DATA_HOME}" && git clone https://github.com/lincheney/fzf-tab-completion)
+fi
+
+source "${XDG_DATA_HOME}/zsh-abbr/zsh-abbr.zsh"
+source "${XDG_DATA_HOME}/fzf-tab-completion/zsh/fzf-zsh-completion.sh"
+bindkey '^I' fzf_completion
